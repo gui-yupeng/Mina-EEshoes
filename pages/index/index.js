5c6b55feb50d447030a02518fca9d99eedc906c1
gui-yupeng <1413137503@qq.com>
2021-05-23 13:56:01 +0800

    显示蓝牙接受到的特征值

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: '欢迎使用EE-shoes',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
